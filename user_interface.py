9967282335f3a30aec5240d4e4c060beb08d9144
Kaamio <mtkotira@gmail.com>
2020-02-02 18:23:26 +0200

    Added wordcloud

--- user_interface.py
+++ user_interface.py
@@ -2,8 +2,8 @@ import dota_main
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-#from PIL import Image
-import WordCloud, STOPWORDS, ImageColorGenerator
+from PIL import Image
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
 def herodata(df,hero): 
     #function returns the rows from the dataframe where hero_id = hero (given as argument)
@@ -32,6 +32,7 @@ while(True):
     print("1 - Hero stats")
     print("2 - Game stats")
     print("3 - Player stats")
+    print("4 - Display wordcloud of chat")
 
     selection = 0
     while selection not in [1,2,3]: 
@@ -44,6 +45,8 @@ while(True):
             break
 
         if selection == 4:
-            wc_data = player_instance.wordCloud() 
-            wc = WordCloud(background_color="white", max_words=10).generate
+            wc_data = player_instance.wordCloud()            
+            list_of_words = (wc_data.get("my_word_counts"))
+            wc = WordCloud(background_color="white",width=1000,height=1000, max_words=20,relative_scaling=0.5,normalize_plurals=False).generate_from_frequencies(list_of_words) 
             plt.imshow(wc)
+            plt.show()
